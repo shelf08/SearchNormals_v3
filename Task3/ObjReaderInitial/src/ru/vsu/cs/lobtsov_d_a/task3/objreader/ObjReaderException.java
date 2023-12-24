232a581aa26cbff27e23ba2c0941532295a0ca8f
shelf08 <vip.lobtsov@gmail.com>
2023-12-24 17:54:11 +0300

    some changes

--- Task3/ObjReaderInitial/src/ru/vsu/cs/lobtsov_d_a/task3/objreader/ObjReaderException.java
+++
@@ -1,7 +0,0 @@
-package ru.vsu.cs.lobtsov_d_a.task3.objreader;
-
-public class ObjReaderException extends RuntimeException {
-    public ObjReaderException(String errorMessage, int lineInd) {
-        super("Error parsing OBJ file on line: " + lineInd + ". " + errorMessage);
-    }
-}
